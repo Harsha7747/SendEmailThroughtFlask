44b761d6847f9b86824cbf7b6f6b708c5b7dbe4f
Harsha Vardhan Gumparthi <147404222+Harsha7747@users.noreply.github.com>
2026-02-23 17:58:45 +0530

    Add files via upload

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn pythonbackend:app 
+web: python main.py
---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+import smtplib
+app=Flask(__name__)
+CORS(app)
+@app.route('/sendemail',methods=['POST'])
+def SendEamail():
+  data=request.get_json()
+  server = smtplib.SMTP('smtp.gmail.com', 587)
+
+# Identify yourself
+  server.ehlo()
+
+# Start TLS encryption
+  server.starttls()
+
+# Login (use app password, not real password)
+  server.login('shyammmmm85@gmail.com', 'wbzs ugwg tncm ddoi')
+
+# Email content
+  from_address = 'shyammmmm85@gmail.com'
+  to_address = 'harshagumpathi1331@gmail.com'
+
+  message=data.get('message') +''+data.get('email')
+
+
+# Send email
+  server.sendmail(from_address, to_address, message)
+
+# Close connection
+  server.quit()
+  return jsonify({
+    "message":"Email send sucessfully"
+  })
+
+  print("Email sent successfully!")
+if __name__=="__main__":
+  app.run(debug=True)
